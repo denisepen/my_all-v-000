451ff8211e1efb9c20d0eaad515689031bfc01ca
Denise Pendleton <dpendl1@hotmail.com>
2018-02-21 23:23:09 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,10 +4,17 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.length
-    yield(collection[i])
+  block_return_values << yield(collection[i])
     i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
   end
 
+end
+
+if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
 
 my_all?([1,2,3]) {|i| i < 2}
