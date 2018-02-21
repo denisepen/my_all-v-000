a30774f1f05fe3d7b0b56e125b33d8cd80588675
Denise Pendleton <dpendl1@hotmail.com>
2018-02-21 23:25:24 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,12 +8,10 @@ def my_all?(collection)
     i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
   end
 
-end
-
 if block_return_values.include?(false)
     false
   else
     true
   end
-
+end
 my_all?([1,2,3]) {|i| i < 2}
